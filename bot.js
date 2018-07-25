b89101d34343e70b56011e00d1ef1258029bbd2d
lo7taremee <41573242+lo7taremee@users.noreply.github.com>
2018-07-25 18:11:15 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,57 +1,55 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '.'
-
+const bot = new Discord.Client();
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Comeing Soon `,"http://twitch.tv/S-F")
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
   console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('            ╔[════════════]╗')
+  console.log('              Bot Is Online')
+  console.log('            ╚[════════════]╝')
   console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
 });
-const Discord = require('discord.js');
-const A7MD = new Discord.Client();
 
-console.log("BOT ONLINE");
+console.log("Welcome Again !");
  
-A7MD.on("guildMemberAdd", member => {
+ client.on("guildMemberRemove", member => {
   member.createDM().then(function (channel) {
   return channel.send(` 
-**
-حيااكْ الله  فى سيرفرنا 
-╔╦╦╦═╦╗╔═╦═╦══╦═╗
-║║║║╩╣╚╣═╣║║║║║╩╣
-╚══╩═╩═╩═╩═╩╩╩╩═╝ 
 
-‏نقدم لك دعوة لسموك الراقيَ .
-‏لنحظى بجمال حضورك وتواجدك
-•بتنـورونـا الجميع :wink:
-•اي مشكله توآجهك ولا يهمك. عندك الاداره تفضل ونحل مشكلتك مع آي شخص بالسيرفر.:telephone_receiver:
-•عندك فكره موضوع تفضل شآركنا فيه:jack_o_lantern::gift:
-•واخيرا حييييياكم واكيد وجودكم هو اللي بيدعمنا وراح نكون الافضل.
-شرفنا يا عسل:wink: :kissing_heart:
-                               [ https://discord.gg/75nGC4 ] 
-الدعووة خاصة لك ... [ ${member}  ]
-**`) 
+
+***دعوه لك خاصه ياحبي***
+***خش ماارح تخسر شى***
+***فله وناسه وكل شى***
+https://discord.gg/xuPegqn
+لاتخيب ظني
+
+
+
+ ${member}  
+`)
+}).catch(console.error)
+
+})
+ 
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+
+
+
+***دعوه لك خاصه ياحبي***
+***خش ماارح تخسر شى***
+***فله وناسه وكل شى***
+https://discord.gg/xuPegqn
+لاتخيب ظني
+
+
+ ${member}  
+`) 
 }).catch(console.error)
 })
 
-client.login(process.env.BOT_TOKEN);
+
+
+
+
+client.login('  توكن الحساب   ');
